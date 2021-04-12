676cbe269180c044578faa39ce218d00cd5e7dcc
Yeis <jcrockyeis@gmail.com>
2021-04-11 22:35:34 -0700

    Project Setup

---
+++ Assets/Scripts/SinusoidalMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinusoidalMove : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5f;
+
+    [SerializeField]
+    float frequency = 20f;
+
+    [SerializeField]
+    float magnitude = 0.5f;
+
+    public float offset = 0.0f;
+
+    private Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveRight();
+    }
+
+    void MoveRight() {
+        position += transform.right * Time.deltaTime * moveSpeed;
+        transform.position = position + transform.up * Mathf.Sin(Time.time * frequency + offset) * magnitude;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject projectile;
+
+    [SerializeField]
+    int totalProjectiles = 10;
+
+    [SerializeField]
+    float delayBetweenProjectiles = 1f;
+
+    private float timeSinceLastSpawned = 0.0f;
+    private int projectilesSpawned = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if(timeSinceLastSpawned >= delayBetweenProjectiles &&
+            totalProjectiles > 0) {
+            totalProjectiles--;
+            timeSinceLastSpawned = 0.0f;
+
+            projectile.GetComponent<SinusoidalMove>().offset += 1; 
+            Instantiate(projectile);
+            }
+    }
+}
