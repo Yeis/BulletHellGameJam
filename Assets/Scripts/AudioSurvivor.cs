1a97efda4ccd2ff533571125b96c1d47a3663557
Rodolfo <rodolfohev@gmail.com>
2021-04-21 23:27:45 -0700

    music wont reload

---
+++ Assets/Scripts/AudioSurvivor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSurvivor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private static AudioSurvivor instance = null;
+    public static AudioSurvivor Instance
+    {
+        get { return instance; }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Awake()
+    {
+        if (instance != null & instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
